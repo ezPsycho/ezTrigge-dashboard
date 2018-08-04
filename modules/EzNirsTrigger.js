49a3d3a357b61510eff02f20b4e8f2e632f45007
Losses Don <losses.don@gmail.com>
2018-08-04 16:29:15 +0800

    Bug fix.

--- modules/EzNirsTrigger.js
+++ modules/EzNirsTrigger.js
@@ -3,6 +3,7 @@ import { i } from '@ez-trigger/server';
 class EzNirsTrigger {
   constructor(server) {
     this.server = server;
+    this.allCommands = ['MK', 'ST', 'EN', 'CL', 'ZR', 'DR', 'LK', 'UL', 'EX', 'RC', 'ER', 'PING']; // prettier-ignore
   }
 
   integrate() {
@@ -20,13 +21,16 @@ class EzNirsTrigger {
           this.broadcast(cmd, client);
         });
       });
+
+    this.server.registerClientTypes({ TRG: 'ezNirsTrigger client' });
+    this.server.registerDebugCommand([ // prettier-ignore
+      'MK', 'CL', 'LK', 'UL', 'EX', 'PING' // prettier-ignore
+    ]); // prettier-ignore
   }
 
   stripe() {
-    ['MK', 'ST', 'EN', 'CL', 'ZR', 'DR', 'LK', 'UL', 'EX', 'RC', 'ER', 'PING'] // prettier-ignore
-      .forEach(cmd => {
-        this.server.commands.deregister(cmd);
-      });
+    this.server.commands.deregister(this.allCommands);
+    this.server.deregisterDebugCommand(this.allCommands);
   }
 
   broadcast(data, client) {
